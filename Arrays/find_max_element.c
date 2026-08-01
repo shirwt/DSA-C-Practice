9f29cf93ceef860ef4f935f7cd2f47c7eb3e5f12
shirwt <shivanirawat0420@gmail.com>
2026-08-01 21:01:30 +0530

    Update find_max_element.c

--- Arrays/find_max_element.c
+++ Arrays/find_max_element.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    //Find the maximum element in an array.
-//Find the minimum element in an array.
-//Calculate the sum of all elements in an array.
 int arr[] = {1,4,2,2};
 int n = sizeof(arr)/sizeof(arr[0]);
 int max=arr[0];
