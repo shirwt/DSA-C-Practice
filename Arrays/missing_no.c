ad51babf02d4b32d5a18536105b54f3bcc8e9b3c
shirwt <shivanirawat0420@gmail.com>
2026-08-08 22:32:32 +0530

    Update missing_no.c

--- Arrays/missing_no.c
+++ Arrays/missing_no.c
@@ -1,13 +1,26 @@
+
+
 #include <stdio.h>
 
 int main() {
-    int arr[] = {2,4, 8, 10};
+    int arr[] = {1, 3};
     int n = sizeof(arr)/sizeof(arr[0]);
-    int diff = arr[1] - arr[0];
+    int actual_diff=-1;
+    int i, j;
+    for(i=0; i<n-1; i++) {
+        int diff = arr[i+1] - arr[i];
+        
+        for(j=0; j<n-1; j++) {
+            int diff_2 = arr[j+1] - arr[j];
+            if(diff == diff_2) {
+                actual_diff = diff;
+            }
+        }
+    }
     int miss;
     for (int i=0; i<n-1; i++) {
-        if(arr[i+1] - arr[i] != diff) {
-             miss = arr[i+1] - diff;
+        if(arr[i+1] - arr[i] != actual_diff) {
+             miss = arr[i+1] - actual_diff;
         }
     }
     
@@ -15,5 +28,3 @@ int main() {
 
     return 0;
 }
-
-// halfway done
