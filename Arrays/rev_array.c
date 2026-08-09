205e88aa3c448d375c696e11cce84eefbc9d522f
shirwt <shivanirawat0420@gmail.com>
2026-08-09 12:57:33 +0530

    Create rev_array.c

---
+++ Arrays/rev_array.c
@@ -0,0 +1,39 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int reverse(int arr[], int n, int arr_2[])
+{
+    int i;
+    
+    for(i=n-1; i>=0; i--) {
+        arr_2[n-1-i] = arr[i];
+    }
+    return 0;
+}
+int main() {
+    
+    int n;
+    
+    printf("enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    int i;
+    for (i=0; i<n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+     int arr_2[n];
+    
+    reverse(arr,n,arr_2);
+    
+     for(i=0; i<n; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    printf("\n");
+    
+    for(i=0; i<n; i++) {
+        printf("%d ", arr_2[i]);
+    }
+
+    return 0;
+}
